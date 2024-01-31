718530e9e79c41a9368d4fecad287053d5095220
johanne <joseznec@hotmail.fr>
2024-01-31 17:54:25 +0100

    Conditional display of Edit Button (CSS and admin.js

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -3,14 +3,12 @@ const editingBanner = document.querySelector(".editing_banner");
 const loginLink = document.querySelector(".login_link");
 const logoutLink = document.querySelector(".logout_link");
 const blockCategories = document.querySelector(".categories");
-console.log(blockCategories);
+const editButton = document.querySelector(".edit_button");
 
 if (isLoggedIn) {
   editingBanner.style.display = "flex";
   loginLink.style.display = "none";
   logoutLink.style.display = "flex";
   blockCategories.style.display = "none";
-  console.log("logué");
-} else {
-  console.log("pas logué");
+  editButton.style.display = "flex";
 }
