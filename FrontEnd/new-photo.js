67269b00b84edfa1f302dd2142059a3d88685759
johanne <joseznec@hotmail.fr>
2024-02-14 18:46:54 +0100

    Displaying the new image in the div class modal-new-photo

--- FrontEnd/new-photo.js
+++ FrontEnd/new-photo.js
@@ -1,10 +1,37 @@
 const modal2 = document.getElementById("modal2");
 const addButton = document.querySelector(".add_photo_button");
+const addButton2 = document.querySelector(".add_photo_button2");
+const inputPicture = document.querySelector("#input-picture");
+const pictureSelection = document.querySelector(".picture-selection");
+const picturePreview = document.querySelector("#picture-preview");
+const selectCategory = document.querySelector("#select-category");
 const closeButton2 = document.querySelector(".close-button2");
 
 // Ouvrir la modal2 lorsqu'on clique sur "Ajouter une photo"
 addButton.addEventListener("click", () => {
   modal2.style.display = "block";
+});
+
+// Ouvrir la boite de téléchargement de la nouvelle image
+addButton2.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputPicture.click();
+});
+
+// Afficher la nouvelle image
+inputPicture.addEventListener("change", () => {
+  const file = inputPicture.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    picturePreview.src = reader.result;
+    pictureSelection.style.display = "none";
+    picturePreview.style.display = "block";
+  };
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
 });
 
 // Revenir sur la modal1 au clic sur la flèche gauche
@@ -17,10 +44,5 @@ arrowLeft.addEventListener("click", () => {
 
 // Fermer la modale lorsqu'on clique sur la croix
 closeButton2.addEventListener("click", () => {
-  modal2.style.display = "none";
-});
-
-// Fermer la modale lorsqu'on clique en dehors de la modale
-modal2.addEventListener("click", () => {
   modal2.style.display = "none";
 });
