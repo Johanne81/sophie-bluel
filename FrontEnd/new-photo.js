f840574052c5fe20a5d999bc28b1a0d09edf70ee
johanne <joseznec@hotmail.fr>
2024-02-15 15:24:07 +0100

    Modal2 : Added file type and size validation

--- FrontEnd/new-photo.js
+++ FrontEnd/new-photo.js
@@ -1,4 +1,4 @@
-const modal2 = document.getElementById("modal2");
+const modal2 = document.querySelector("#modal2");
 const addButton = document.querySelector(".add_photo_button");
 const addButton2 = document.querySelector(".add_photo_button2");
 const inputPicture = document.querySelector("#input-picture");
@@ -12,28 +12,6 @@ addButton.addEventListener("click", () => {
   modal2.style.display = "block";
 });
 
-// Ouvrir la boite de téléchargement de la nouvelle image
-addButton2.addEventListener("click", (e) => {
-  e.preventDefault();
-  inputPicture.click();
-});
-
-// Afficher la nouvelle image
-inputPicture.addEventListener("change", () => {
-  const file = inputPicture.files[0];
-  const reader = new FileReader();
-
-  reader.onload = () => {
-    picturePreview.src = reader.result;
-    pictureSelection.style.display = "none";
-    picturePreview.style.display = "block";
-  };
-
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-});
-
 // Revenir sur la modal1 au clic sur la flèche gauche
 const arrowLeft = document.querySelector(".fa-arrow-left");
 arrowLeft.addEventListener("click", () => {
@@ -45,4 +23,38 @@ arrowLeft.addEventListener("click", () => {
 // Fermer la modale lorsqu'on clique sur la croix
 closeButton2.addEventListener("click", () => {
   modal2.style.display = "none";
+});
+
+// Ouvrir la boite de téléchargement de la nouvelle image
+addButton2.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputPicture.click();
+});
+
+// Afficher la nouvelle image
+inputPicture.addEventListener("change", () => {
+  const file = inputPicture.files[0];
+  const reader = new FileReader();
+
+  // Vérifier le type de fichier
+  const acceptedTypes = ["image/jpeg", "image/png"];
+  if (!acceptedTypes.includes(file.type)) {
+    alert("Seuls les fichiers JPG et PNG sont autorisés.");
+    return;
+  }
+  // Vérifier la taille du fichier
+  const maxSize = 4 * 1024 * 1024; // 4 Mo
+  if (file.size > maxSize) {
+    alert("La taille du fichier ne doit pas dépasser 4 Mo.");
+    return;
+  }
+  reader.onload = () => {
+    picturePreview.src = reader.result;
+    pictureSelection.style.display = "none";
+    picturePreview.style.display = "block";
+  };
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
 });
